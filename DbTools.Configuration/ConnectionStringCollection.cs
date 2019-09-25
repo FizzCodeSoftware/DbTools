8c3dcc47ebc2d0e3cdbbe1912ca7050be3cbf4b4
Lengyel János Zoltán <lengyel.janos@outlook.com>
2019-09-25 13:08:54 +0200

    add FizzCode.DbTools.Configuration assembly

---
+++ DbTools.Configuration/ConnectionStringCollection.cs
@@ -0,0 +1,40 @@
+﻿namespace FizzCode.DbTools.Configuration
+{
+    using System.Collections.Generic;
+    using Microsoft.Extensions.Configuration;
+
+    public class ConnectionStringCollection
+    {
+        private readonly Dictionary<string, ConnectionStringWithProvider> _connectionStrings = new Dictionary<string, ConnectionStringWithProvider>();
+        public IEnumerable<ConnectionStringWithProvider> All => _connectionStrings.Values;
+
+        public void LoadFromConfiguration(IConfigurationRoot configuration, string path = "ConnectionStrings")
+        {
+            var connectionStrings = configuration
+                .GetSection(path)
+                .Get<ConnectionStringWithProvider[]>();
+
+            if (connectionStrings == null)
+                return;
+
+            foreach (var connectionString in connectionStrings)
+            {
+                Add(connectionString);
+            }
+        }
+
+        public void Add(ConnectionStringWithProvider connectionString)
+        {
+            _connectionStrings[connectionString.Name] = connectionString;
+        }
+
+        public ConnectionStringWithProvider this[string name]
+        {
+            get
+            {
+                _connectionStrings.TryGetValue(name, out var value);
+                return value;
+            }
+        }
+    }
+}
---
+++ DbTools.Configuration/ConnectionStringWithProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace FizzCode.DbTools.Configuration
+{
+    public class ConnectionStringWithProvider
+    {
+        public string Name { get; set; }
+        public string Provider { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
