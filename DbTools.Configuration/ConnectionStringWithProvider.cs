6f6060e322a735abea41159b5ca7b0a5e5ff902d
Lengyel János Zoltán <lengyel.janos@outlook.com>
2019-09-25 14:46:50 +0200

    fix ProviderName field

--- DbTools.Configuration/ConnectionStringWithProvider.cs
+++ DbTools.Configuration/ConnectionStringWithProvider.cs
@@ -3,7 +3,7 @@
     public class ConnectionStringWithProvider
     {
         public string Name { get; set; }
-        public string Provider { get; set; }
+        public string ProviderName { get; set; }
         public string ConnectionString { get; set; }
     }
 }
