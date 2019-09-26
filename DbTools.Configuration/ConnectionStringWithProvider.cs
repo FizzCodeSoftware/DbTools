afccde2340e180c44688a60db11c6f9877b65e68
Lengyel János Zoltán <lengyel.janos@outlook.com>
2019-09-26 09:30:00 +0200

    add DebuggerDisplay and ToString to ConnectionStringWithProvider

--- DbTools.Configuration/ConnectionStringWithProvider.cs
+++ DbTools.Configuration/ConnectionStringWithProvider.cs
@@ -1,9 +1,18 @@
 ﻿namespace FizzCode.DbTools.Configuration
 {
+    using System.Diagnostics;
+    using System.Globalization;
+
+    [DebuggerDisplay("{Name}, {ProviderName}, {ConnectionString}")]
     public class ConnectionStringWithProvider
     {
         public string Name { get; set; }
         public string ProviderName { get; set; }
         public string ConnectionString { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0}, {1}, {2}", Name, ProviderName, ConnectionString);
+        }
     }
 }
