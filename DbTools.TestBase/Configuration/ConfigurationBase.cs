f04fa39e1872df19045c2b2916719b6b37d8ea26
gerleim <gerleim@gmail.com>
2019-10-16 15:10:06 +0200

    Fix Configuration handling.

---
+++ DbTools.TestBase/Configuration/ConfigurationBase.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzCode.DbTools.TestBase
+{
+    using FizzCode.DbTools.Configuration;
+    using Microsoft.Extensions.Configuration;
+
+    public class ConfigurationBase
+    {
+        public ConfigurationBase()
+        {
+            Initialize();
+        }
+
+        public ConfigurationBase(string configurationFileName)
+        {
+            ConfigurationFileName = configurationFileName;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Configuration = Common.Configuration.Load(ConfigurationFileName);
+            ConnectionStrings.LoadFromConfiguration(Configuration);
+        }
+
+        public virtual string ConfigurationFileName { get; }
+
+        public IConfigurationRoot Configuration { get; private set; }
+
+        public ConnectionStringCollection ConnectionStrings { get; } = new ConnectionStringCollection();
+    }
+}
---
+++ DbTools.TestBase/Configuration/StaticConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace FizzCode.DbTools.TestBase
+{
+    using FizzCode.DbTools.Configuration;
+    using Microsoft.Extensions.Configuration;
+
+    public static class StaticConfiguration
+    {
+        private static ConfigurationBase _configuration;
+
+        public static void Initialize(string configurationFileName)
+        {
+            if (_configuration == null)
+                _configuration = new ConfigurationBase(configurationFileName);
+            else
+                if (configurationFileName != _configuration.ConfigurationFileName)
+                    throw new System.InvalidOperationException("Already initialized.");
+        }
+
+        public static IConfigurationRoot Configuration
+        {
+            get
+            {
+                return _configuration.Configuration;
+            }
+        }
+
+        public static ConnectionStringCollection ConnectionStrings { get; } = new ConnectionStringCollection();
+    }
+}
