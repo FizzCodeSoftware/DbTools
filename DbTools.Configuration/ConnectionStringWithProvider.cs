d22ddd0c79b0af5968613a8b561193c0d9efe9ef
Lengyel János Zoltán <lengyel.janos@outlook.com>
2019-10-17 10:49:47 +0200

    add GetKnownConnectionStringFields to ConnectionStringWithProvider

--- DbTools.Configuration/ConnectionStringWithProvider.cs
+++ DbTools.Configuration/ConnectionStringWithProvider.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Diagnostics;
     using System.Globalization;
+    using System.Linq;
 
     [DebuggerDisplay("{Name}, {ProviderName}, {ConnectionString}")]
     public class ConnectionStringWithProvider
@@ -41,6 +42,58 @@
                 KnownProvider = Configuration.KnownProvider.MySql;
             else if (string.Equals(ProviderName, "Npgsql", StringComparison.InvariantCultureIgnoreCase))
                 KnownProvider = Configuration.KnownProvider.PgSql;
+        }
+
+        public ConnectionStringFields GetKnownConnectionStringFields()
+        {
+            if (string.IsNullOrEmpty(ConnectionString))
+                return null;
+
+            var values = ConnectionString
+                .Split(';', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim());
+
+            var result = new ConnectionStringFields();
+
+            foreach (var v in values)
+            {
+                var idx = v.IndexOf("=");
+                if (idx == -1)
+                    continue;
+
+                var name = v.Substring(0, idx).ToLowerInvariant();
+                var value = v.Substring(idx + 1);
+                switch (name)
+                {
+                    case "server":
+                    case "data source":
+                        result.Server = value;
+                        // todo: support oracle's complex Data Source format:
+                        /*if (KnownProvider == Configuration.KnownProvider.Oracle)
+                        {
+                            // Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));
+                        }*/
+                        break;
+                    case "database":
+                    case "initial catalog":
+                        result.Database = value;
+                        break;
+                    case "user id":
+                    case "uid":
+                        result.UserId = value;
+                        break;
+                    case "port":
+                        if (int.TryParse(value, out var port))
+                            result.Port = port;
+                        break;
+                    case "integratedsecurity":
+                    case "integrated security":
+                        result.IntegratedSecurity = string.Equals(value, "yes", StringComparison.InvariantCultureIgnoreCase)
+                            || string.Equals(value, "sspi", StringComparison.InvariantCultureIgnoreCase);
+                        break;
+                }
+            }
+            return result;
         }
     }
 }
