2de040922c12cd759abca4bf5baf939ec427dfa3
Lengyel János Zoltán <lengyel.janos@outlook.com>
2019-10-17 11:00:47 +0200

    fix ConnectionStringFields

--- DbTools.Configuration/ConnectionStringFields.cs
+++ DbTools.Configuration/ConnectionStringFields.cs
@@ -3,9 +3,9 @@
     public class ConnectionStringFields
     {
         public string Server { get; set; }
-        public int Port { get; set; }
+        public int? Port { get; set; }
         public string Database { get; set; }
         public string UserId { get; set; }
-        public bool IntegratedSecurity { get; set; }
+        public bool? IntegratedSecurity { get; set; }
     }
 }
