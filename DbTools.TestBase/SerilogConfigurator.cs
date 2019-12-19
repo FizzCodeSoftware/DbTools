a3aacf6ff884c2edde8ba7f85fe1f689fbace2c1
gerleim <gerleim@gmail.com>
2019-12-19 13:35:11 +0100

    Fix Log format, Level as 3 chars.

--- DbTools.TestBase/SerilogConfigurator.cs
+++ DbTools.TestBase/SerilogConfigurator.cs
@@ -14,7 +14,7 @@
 
             var config = new LoggerConfiguration()
                 .Enrich.WithExceptionDetails()
-                .WriteTo.Debug(minimumLevel, "{Timestamp:HH:mm:ss.fff} [{Level}] {Message} {Properties}{NewLine}{Exception}");
+                .WriteTo.Debug(minimumLevel, "{Timestamp:HH:mm:ss.fff} [{Level:u3}] {Message} {Properties}{NewLine}{Exception}");
 
             config = config.MinimumLevel.Is(minimumLevel);
 
