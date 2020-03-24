568995b93499ed90fd9db876b5eb141f8ad5ec76
Lengyel János Zoltán <lengyel.janos@outlook.com>
2020-03-24 14:15:25 +0100

    long FK/PK naming fixes, separation, new defaults

---
+++ DbTools.DataDefinition.MsSql/NamingStrategies/PrimaryKeyNamingMsSqlDefaultStrategy.cs
@@ -0,0 +1,22 @@
+﻿namespace FizzCode.DbTools.DataDefinition
+{
+    using System;
+    using System.Globalization;
+
+    public class PrimaryKeyNamingMsSqlDefaultStrategy : IPrimaryKeyNamingStrategy
+    {
+        public void SetPrimaryKeyName(PrimaryKey pk)
+        {
+            if (pk.SqlTable.SchemaAndTableName?.TableName == null)
+                return;
+
+            var pkName = $"PK_{pk.SqlTable.SchemaAndTableName.TableName}";
+            if (pkName.Length > 110)
+            {
+                pkName = pkName.Substring(0, 110) + "__" + pkName.GetHashCode(StringComparison.InvariantCultureIgnoreCase).ToString("D", CultureInfo.InvariantCulture);
+            }
+
+            pk.Name = pkName;
+        }
+    }
+}
---
+++ DbTools.DataDefinition/DatabaseDeclaration/NamingStrategies/IndexNamingDefaultStrategy.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzCode.DbTools.DataDefinition
+{
+    using System.Linq;
+
+    public class IndexNamingDefaultStrategy : IIndexNamingStrategy
+    {
+        public void SetIndexName(Index index)
+        {
+            if (index.SqlTable.SchemaAndTableName.TableName == null)
+                return;
+
+            var indexNameColumnsPart = string.Join("_", index.SqlColumns.Select(co => co.SqlColumn.Name).ToList());
+            index.Name = $"IX_{index.SqlTable.SchemaAndTableName.TableName}_{indexNameColumnsPart}";
+        }
+    }
+}
