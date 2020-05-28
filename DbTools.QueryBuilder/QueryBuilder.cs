156a32e616604fa2c0142f78b3914c00921fd808
gerleim <gerleim@gmail.com>
2020-05-28 11:47:19 +0200

    Add new query and test projects.

---
+++ DbTools.QueryBuilder/QueryBuilder.cs
@@ -0,0 +1,27 @@
+﻿namespace FizzCode.DbTools.QueryBuilder
+{
+    using System.Collections.Generic;
+    using FizzCode.DbTools.DataDefinition;
+
+    public class QueryBuilder
+    {
+        public string Build(Query query)
+        {
+            return "";
+        }
+    }
+
+    public class Query : QueryElement
+    {
+    }
+
+    public class Join
+    {
+    }
+
+    public class QueryElement
+    {
+        public SqlTable Table { get; set; }
+        public List<SqlColumn> QueryColumns { get; set; }
+    }
+}
---
+++ Tests/DbTools.QueryBuilder.Tests/QueryBuilderTests.cs
@@ -0,0 +1,27 @@
+﻿namespace FizzCode.DbTools.QueryBuilder.Tests
+{
+    using FizzCode.DbTools.DataDefinition.Tests;
+    using FizzCode.DbTools.QueryBuilder;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class QueryBuilderTests
+    {
+        [TestMethod]
+        public void BuildTest()
+        {
+            /* simple example
+             * pull parent field with child
+             */
+
+            var db = new TestDatabaseFks();
+
+            var qb = new QueryBuilder();
+
+            var q = new Query();
+            // q.Table = db()
+
+            var result = qb.Build(q);
+        }
+    }
+}
